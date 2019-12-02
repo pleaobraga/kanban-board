4078131c910df4b245c8de43cbce141828ce41a4
Pedro Braga <plbraga@mmsi.com>
2019-12-02 11:05:51 -0300

    Added Server

---
+++ server/server.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+const Joi = require("@hapi/joi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World!";
+    }
+  });
+
+  server.route({
+    method: "POST",
+    path: "/",
+    config: {
+      validate: {
+        payload: Joi.object({
+          lists: Joi.array().items(
+            Joi.object({
+              id: Joi.string().required(),
+              title: Joi.string().required(),
+              cards: Joi.array().items(
+                Joi.object({
+                  id: Joi.string().required(),
+                  type: Joi.string().required(),
+                  duration: Joi.number().required(),
+                  severity: Joi.string().required()
+                })
+              )
+            })
+          )
+        }),
+        failAction: (request, h, error) => {
+          throw error;
+        }
+      }
+    },
+    handler: (request, h) => {
+      return request.payload;
+    }
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
