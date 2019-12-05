b45d2986f2e2dcde8bdbdd872e8c398068e5a506
Pedro <pleao.braga@gmail.com>
2019-12-05 18:44:56 -0300

    Updated Tests

--- client/src/reducers/BoardReducer/BoardReducer.test.js
+++ client/src/reducers/BoardReducer/BoardReducer.test.js
@@ -1,152 +1,291 @@
 jest.mock('../../utils/utils')
 import { initialStateReducer, mockStore } from '../__mocks__/reduxMock'
 import * as constant from '../../utils/constants'
-import contentReducer, {
+import contentReducer from './BoardReducer'
+import {
   getBoardStart,
   getBoardError,
-  getBoardSuccess
-  // getBoard
-} from './BoardReducer'
+  getBoardSuccess,
+  postBoardStart,
+  postBoardError,
+  postBoardSuccess,
+  putBoardStart,
+  putBoardError,
+  putBoardSuccess
+} from './BorderActions'
 
 describe('Board Redux', () => {
   it('should return the initial state', () => {
     expect(contentReducer(undefined, {})).toEqual(initialStateReducer)
   })
 
-  describe('Start to get the content', () => {
-    const payload = { type: constant.GET_BOARD }
+  describe('GET BOARD', () => {
+    describe('Start to get the board', () => {
+      const payload = { type: constant.GET_BOARD }
 
-    it('should dispatch getBoardStart', () => {
-      const store = mockStore(initialStateReducer)
+      it('should dispatch getBoardStart', () => {
+        const store = mockStore(initialStateReducer)
 
-      store.dispatch(getBoardStart())
+        store.dispatch(getBoardStart())
 
-      const actions = store.getActions()
-      expect(actions).toEqual([payload])
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.GET_BOARD}`, () => {
+        const actualState = { ...initialStateReducer, loadingContent: true }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
     })
 
-    it(`should handle ${constant.GET_BOARD}`, () => {
-      const actualState = { ...initialStateReducer, loadingContent: true }
+    describe('Error to get the board', () => {
+      const error = 'Error'
+      const payload = { type: constant.GET_BOARD_ERROR, error }
 
-      expect(contentReducer(undefined, payload)).toEqual(actualState)
-    })
-  })
+      it('should dispatch getBoardError', () => {
+        const store = mockStore(initialStateReducer)
 
-  describe('Error to get the content', () => {
-    const error = 'Error'
-    const payload = { type: constant.GET_BOARD_ERROR, error }
+        store.dispatch(getBoardError(error))
 
-    it('should dispatch getBoardError', () => {
-      const store = mockStore(initialStateReducer)
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
 
-      store.dispatch(getBoardError(error))
-
-      const actions = store.getActions()
-      expect(actions).toEqual([payload])
-    })
-
-    it(`should handle ${constant.GET_BOARD_ERROR}`, () => {
-      const actualState = {
-        ...initialStateReducer,
-        loadingContent: false,
-        errorContent: true
-      }
-
-      expect(contentReducer(undefined, payload)).toEqual(actualState)
-    })
-  })
-
-  describe('Success to get content', () => {
-    const board = {
-      lists: [
-        {
-          id: '0',
-          name: 'Backlog',
-          cards: [
-            {
-              id: 'test1',
-              type: 'feature',
-              duration: 1,
-              severity: 'hight'
-            }
-          ]
-        },
-        {
-          id: '1',
-          name: 'To DO',
-          cards: []
+      it(`should handle ${constant.GET_BOARD_ERROR}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          loadingContent: false,
+          errorContent: true
         }
-      ]
-    }
-    const payload = { type: constant.GET_BOARD_SUCCESS, board }
 
-    it('should dispatch getBoardSuccess', () => {
-      const store = mockStore(initialStateReducer)
-
-      store.dispatch(getBoardSuccess(board))
-
-      const actions = store.getActions()
-      expect(actions).toEqual([payload])
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
     })
 
-    it(`should handle ${constant.GET_BOARD_SUCCESS}`, () => {
-      const actualState = {
-        ...initialStateReducer,
-        board: board,
-        loadingContent: false,
-        errorContent: false
+    describe('Success to get board', () => {
+      const board = {
+        lists: [
+          {
+            id: '0',
+            name: 'Backlog',
+            cards: [
+              {
+                id: 'test1',
+                type: 'feature',
+                duration: 1,
+                severity: 'hight'
+              }
+            ]
+          },
+          {
+            id: '1',
+            name: 'To DO',
+            cards: []
+          }
+        ]
       }
+      const payload = { type: constant.GET_BOARD_SUCCESS, board }
 
-      expect(contentReducer(undefined, payload)).toEqual(actualState)
+      it('should dispatch getBoardSuccess', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(getBoardSuccess(board))
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.GET_BOARD_SUCCESS}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          board: board,
+          loadingContent: false,
+          errorContent: false
+        }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
     })
   })
 
-  // describe('Get the content', () => {
-  //   it('Success to get the content', () => {
-  //     const store = mockStore(initialStateReducer)
+  describe('POST BOARD', () => {
+    describe('Start to post the board', () => {
+      const payload = { type: constant.POST_BOARD }
 
-  //     return store.dispatch(getBoard()).then(() => {
-  //       const board = {
-  //         lists: [
-  //           {
-  //             id: '0',
-  //             name: 'Backlog',
-  //             cards: [
-  //               {
-  //                 id: 'test1',
-  //                 type: 'feature',
-  //                 duration: 1,
-  //                 severity: 'hight'
-  //               },
-  //               {
-  //                 id: 'test2',
-  //                 type: 'research',
-  //                 duration: 2,
-  //                 severity: 'medium'
-  //               },
-  //               {
-  //                 id: 'test3',
-  //                 type: 'content',
-  //                 duration: 3,
-  //                 severity: 'low'
-  //               }
-  //             ]
-  //           },
-  //           {
-  //             id: '1',
-  //             name: 'To DO',
-  //             cards: []
-  //           }
-  //         ]
-  //       }
-  //       const payload = [
-  //         { type: constant.GET_BOARD },
-  //         { type: constant.GET_BOARD_SUCCESS, board }
-  //       ]
+      it('should dispatch postBoardStart', () => {
+        const store = mockStore(initialStateReducer)
 
-  //       const actions = store.getActions()
-  //       expect(actions).toEqual(payload)
-  //     })
-  //   })
-  // })
+        store.dispatch(postBoardStart())
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.POST_BOARD}`, () => {
+        const actualState = { ...initialStateReducer, loadingContent: true }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+
+    describe('Error to post the board', () => {
+      const error = 'Error'
+      const payload = { type: constant.POST_BOARD_ERROR, error }
+
+      it('should dispatch postBoardError', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(postBoardError(error))
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.POST_BOARD_ERROR}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          loadingContent: false,
+          errorContent: true
+        }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+
+    describe('Success to post board', () => {
+      const board = {
+        lists: [
+          {
+            id: '0',
+            name: 'Backlog',
+            cards: [
+              {
+                id: 'test1',
+                type: 'feature',
+                duration: 1,
+                severity: 'hight'
+              }
+            ]
+          },
+          {
+            id: '1',
+            name: 'To DO',
+            cards: []
+          }
+        ]
+      }
+      const payload = { type: constant.POST_BOARD_SUCCESS, board }
+
+      it('should dispatch postBoardSuccess', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(postBoardSuccess(board))
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.POST_BOARD_SUCCESS}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          board: board,
+          loadingContent: false,
+          errorContent: false
+        }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+  })
+
+  describe('PUT BOARD', () => {
+    describe('Start to put the board', () => {
+      const payload = { type: constant.PUT_BOARD }
+
+      it('should dispatch putBoardStart', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(putBoardStart())
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.PUT_BOARD}`, () => {
+        const actualState = { ...initialStateReducer, loadingContent: true }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+
+    describe('Error to put the board', () => {
+      const error = 'Error'
+      const payload = { type: constant.PUT_BOARD_ERROR, error }
+
+      it('should dispatch putBoardError', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(putBoardError(error))
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.PUT_BOARD_ERROR}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          loadingContent: false,
+          errorContent: true
+        }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+
+    describe('Success to put board', () => {
+      const board = {
+        TaskLists: [
+          {
+            id: '0',
+            name: 'Backlog',
+            cards: [
+              {
+                id: 'test1',
+                type: 'feature',
+                duration: 1,
+                severity: 'hight'
+              }
+            ]
+          },
+          {
+            id: '1',
+            name: 'To DO',
+            cards: []
+          }
+        ]
+      }
+      const payload = { type: constant.PUT_BOARD_SUCCESS, board }
+
+      it('should dispatch putBoardSuccess', () => {
+        const store = mockStore(initialStateReducer)
+
+        store.dispatch(putBoardSuccess(board))
+
+        const actions = store.getActions()
+        expect(actions).toEqual([payload])
+      })
+
+      it(`should handle ${constant.PUT_BOARD_SUCCESS}`, () => {
+        const actualState = {
+          ...initialStateReducer,
+          board: board,
+          loadingContent: false,
+          errorContent: false
+        }
+
+        expect(contentReducer(undefined, payload)).toEqual(actualState)
+      })
+    })
+  })
 })
--- client/src/utils/utils.test.js
+++ client/src/utils/utils.test.js
@@ -1,11 +1,48 @@
-import { simulateRequest } from './utils'
+import { simulateRequest, findObject, removeObjFromArray } from './utils'
 
 describe('Utils', () => {
-  it('Simulate Request function', () => {
-    const data = { test: 'test' }
+  describe('simulateRequest', () => {
+    it('should Simulate Request function', () => {
+      const data = { test: 'test' }
 
-    return simulateRequest(data).then(resp => {
-      expect(resp).toEqual({ data })
+      return simulateRequest(data).then(resp => {
+        expect(resp).toEqual({ data })
+      })
+    })
+  })
+
+  describe('findObject', () => {
+    it('should find object', () => {
+      const array = [{ index: 0 }, { index: 1 }, { index: 2 }]
+      const property = 'index'
+      const value = 1
+
+      const answer = { index: 1 }
+
+      const result = findObject(array, property, value)
+      expect(result).toEqual(answer)
+    })
+  })
+
+  describe('removeObjFromArray', () => {
+    it('should remove oject from array', () => {
+      const array = [{ index: 0 }, { index: 1 }, { index: 2 }]
+      const obj = { index: 1 }
+
+      const answer = [{ index: 0 }, { index: 2 }]
+
+      const result = removeObjFromArray(array, obj)
+      expect(result).toEqual(answer)
+    })
+
+    it('should not remove oject from array', () => {
+      const array = [{ index: 0 }, { index: 1 }, { index: 2 }]
+      const obj = { index: 4 }
+
+      const answer = undefined
+
+      const result = removeObjFromArray(array, obj)
+      expect(result).toEqual(answer)
     })
   })
 })
